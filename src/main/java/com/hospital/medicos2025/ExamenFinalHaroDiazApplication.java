5c7fb970091ac651df1c592d5b4fb8976a09f7a7
Irvin D.Haro Diaz <i202334997@cibertec.edu.pe>
2025-02-19 19:07:41 -0500

    primer commit

---
+++ src/main/java/com/hospital/medicos2025/ExamenFinalHaroDiazApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.medicos2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalHaroDiazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFinalHaroDiazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospital/medicos2025/ExamenFinalHaroDiazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.medicos2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalHaroDiazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
